58312053cf7ce8e5d605a47d52fa5b846389aa0b
RomeroFederico <romerof14@gmail.com>
2016-03-18 17:29:24 -0300

    Creacion del programa.

---
+++ Ejercicio_8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_8
+{
+    class Ejercicio_8
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro. 8";
+        }
+    }
+}
